387b8fa26a401f6e4948f5bf9940a2ae5d5c777f
hendrywilliam <hendriwilliam29@gmail.com>
2022-12-05 14:24:59 +0700

    ver 0.1.1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [    "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "bg-dark": "#17345F",
+      "bg-light": "#F4F7FB",
+      "bg-white": "#FFFFFF",
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
-}
+};
