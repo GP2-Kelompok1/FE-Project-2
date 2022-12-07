98461fc732c4e7621d3c91ea67fbbcbeef8fa0c2
hendrywilliam <hendriwilliam29@gmail.com>
2022-12-07 07:55:15 +0700

    done container buat setiap halaman list

--- src/pages/MenteeList.jsx
+++ src/pages/MenteeList.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import NavBar from "../components/NavBar";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 const MenteeList = () => {
+  const [cookies, removeCookie, setCookie] = useCookies();
+  const navi = useNavigate();
+  const handleLogout = () => {
+    removeCookie("Username", { path: "/" });
+    removeCookie("Pass", { path: "/" });
+    navi("/");
+  };
   return (
-    <div>MenteeList</div>
-  )
-}
+    <div className="flex flex-row bg-alta-white w-screen h-screen">
+      <div>
+        <Sidebar />
+      </div>
+      <div className="w-full h-full bg-alta-light">
+        <NavBar user={cookies.Username} logout={handleLogout} />
+        <div className="bg-alta-white h-5/6 m-5">
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MenteeList
+export default MenteeList;
